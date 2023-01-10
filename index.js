bcc55e91a966176590112ef891c41843c421dbe3
BewSons <bewpinn55@gmail.com>
2023-01-10 22:48:13 +0700

    First Commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const {ApolloServer} = require('apollo-server-express')
+const mongoose = require('mongoose')
+const typeDefs = require('./typeDefs')
+const resolvers = require('./resolvers')
+
+// Database Connection
+const URL= "mongodb+srv://new-user31:new-user31@labnodeapi.16lbmil.mongodb.net/mySecondDatabase?retryWrites=true&w=majority"
+
+mongoose.connect(URL, {
+    useUnifiedTopology:true, useNewUrlParser:true
+}, ()=>console.log("DB CONNECTED"))
+
+
+
+const startServer = async() => {
+    const app = express ()
+    const apolloServer = new ApolloServer({
+        typeDefs, 
+        resolvers
+    })
+    await apolloServer.start()
+    apolloServer.applyMiddleware({app:app})
+    app.listen(4000,() => console.log("Server Up & Running 4000"))
+}
+
+startServer();
